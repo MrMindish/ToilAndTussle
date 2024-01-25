69436992c50158763b15550370b22bafad386432
Adam Blyth <135326566+MrMindish@users.noreply.github.com>
2024-01-25 13:47:03 +0000

    Fixing Player Movement
    Fixing the issues caused by the hard crash. Also changed the player sprite from a material to a sprite renderer.

---
+++ Assets/Code/PlayerMovement/NewPlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AB
+{
+    public class NewPlayerMovement : MonoBehaviour
+    {
+        PlayerPushBox playerPushBox;
+
+        private float horizontal;
+        public float moveSpeed;
+        public float jumpingPower;
+
+        public string JumpName;
+
+        public GameObject playerOneX;
+        public GameObject playerTwoX;
+        private bool isFacingRight = true;
+
+        [SerializeField] private Rigidbody rb;
+        [SerializeField] private LayerMask groundLayer;
+        [SerializeField] private Transform orientation;
+
+        void Update()
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+        }
+
+        private void FixedUpdate()
+        {
+            rb.velocity = new Vector3(horizontal * moveSpeed, rb.velocity.y, 0);
+        }
+    }
+}
